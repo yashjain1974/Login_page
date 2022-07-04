4c39a202b11a8e804d2ae07921f8afcf031171fc
Yash <yash191174@gmail.com>
2022-07-04 14:48:22 +0530

    modified contxt

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../contextstore/auth-context';
 
 const Navigation = (props) => {
-  return (
-    <AuthContext.Consumer >
-      {(ctx)=>{
+  const ctx=useContext(AuthContext);
+
         return(
          <nav className={classes.nav}>
          <ul>
@@ -22,17 +21,13 @@ const Navigation = (props) => {
            )}
            {ctx.isLoggedIn && (
              <li>
-               <button onClick={props.onLogout}>Logout</button>
+               <button onClick={ctx.onLogout}>Logout</button>
              </li>
            )}
          </ul>
        </nav>
         )
 
-      }}
-   
-    </AuthContext.Consumer>
-  );
 };
 
 export default Navigation;
